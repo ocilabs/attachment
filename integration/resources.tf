bc772fe0caab3ea2c7bb1f7607c303341bfb1ed5
Torsten Boettjer <torsten.boettjer@oracle.com>
2022-02-18 06:57:33 +0100

    initial load

--- integration/resources.tf
+++ integration/resources.tf
@@ -1,23 +1,5 @@
-// Copyright (c) 2017, 2021, Oracle and/or its affiliates. All rights reserved.
-// Licensed under the Mozilla Public License v2.0
-
-variable "tenancy_ocid" {
-}
-
-variable "user_ocid" {
-}
-
-variable "fingerprint" {
-}
-
-variable "private_key_path" {
-}
-
-variable "region" {
-}
-
-variable "compartment_id" {
-}
+# Copyright (c) 2020 Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 variable "integration_instance_idcs_access_token" {
 
@@ -32,16 +14,13 @@ variable allow_listed_http_vcn {
 }
 
 provider "oci" {
-  tenancy_ocid     = var.tenancy_ocid
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
-  region           = var.region
+  tenancy_ocid     = var.tenancy.id
+  region           = var.resident.region.key
 }
 
 resource "oci_integration_integration_instance" "test_integration_instance" {
   #Required
-  compartment_id            = var.compartment_id
+  compartment_id            = var.input.resident.id
   display_name              = "displayName"
   integration_instance_type = "STANDARD"
   is_byol                   = "false"
